0e4a744268d21936b90b7981a802c4812ea647cf
Nadiele <seuemail@gmail.com>
2026-03-12 22:29:47 -0300

    agenda profissional com painel admin

--- agendamento.js
+++ agendamento.js
@@ -1,40 +1,3 @@
-// carregar horários ocupados
-
-function carregarHorarios(){
-
-let data = document.getElementById("data").value
-let horaSelect = document.getElementById("hora")
-
-if(!data) return
-
-let agendamentos = JSON.parse(localStorage.getItem("agendamentos")) || []
-
-for(let option of horaSelect.options){
-
-option.disabled = false
-option.style.background = "#9be2a9"
-
-for(let a of agendamentos){
-
-if(a.data === data && a.hora === option.value){
-
-option.disabled = true
-option.style.background = "#ff7b7b"
-
-}
-
-}
-
-}
-
-}
-
-document.getElementById("data").addEventListener("change", carregarHorarios)
-
-
-
-// enviar agendamento
-
 function enviarWhatsapp(){
 
 let nome = document.getElementById("nome").value
@@ -45,26 +8,24 @@ let hora = document.getElementById("hora").value
 if(!nome || !data){
 
 alert("Preencha seu nome e a data")
+
 return
 
 }
 
 let agendamentos = JSON.parse(localStorage.getItem("agendamentos")) || []
 
-// verificar horário ocupado
-
 for(let a of agendamentos){
 
 if(a.data === data && a.hora === hora){
 
 alert("Esse horário já está ocupado")
+
 return
 
 }
 
 }
-
-// salvar agendamento
 
 agendamentos.push({
 
@@ -75,23 +36,19 @@ hora:hora
 
 })
 
-localStorage.setItem("agendamentos", JSON.stringify(agendamentos))
+localStorage.setItem("agendamentos",JSON.stringify(agendamentos))
 
-let mensagem = `Olá Patricia! Gostaria de agendar.
+let telefone="5582996692302"
+
+let mensagem=`Olá Patricia! Gostaria de agendar.
 
 Nome: ${nome}
 Serviço: ${servico}
 Data: ${data}
 Hora: ${hora}`
 
-let telefone = "5582996692302"
-
-let url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`
+let url=`https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`
 
 window.open(url,"_blank")
 
-carregarHorarios()
-
 }
-
-window.onload = carregarHorarios
