d05dad0449a96e90882d545da9ea2457d1883cc7
Nadiele <seuemail@gmail.com>
2026-03-12 23:12:56 -0300

    painel profissional

--- agendamento.js
+++ agendamento.js
@@ -1,54 +1,26 @@
-function enviarWhatsapp(){
+function carregarHorarios(){
 
-let nome = document.getElementById("nome").value
-let servico = document.getElementById("servico").value
-let data = document.getElementById("data").value
-let hora = document.getElementById("hora").value
+let data=document.getElementById("data").value
+let horaSelect=document.getElementById("hora")
 
-if(!nome || !data){
+let agendamentos=JSON.parse(localStorage.getItem("agendamentos"))||[]
 
-alert("Preencha seu nome e a data")
+for(let option of horaSelect.options){
 
-return
-
-}
-
-let agendamentos = JSON.parse(localStorage.getItem("agendamentos")) || []
+option.disabled=false
 
 for(let a of agendamentos){
 
-if(a.data === data && a.hora === hora){
+if(a.data===data && a.hora===option.value){
 
-alert("Esse horário já está ocupado")
-
-return
+option.disabled=true
 
 }
 
 }
 
-agendamentos.push({
-
-nome:nome,
-servico:servico,
-data:data,
-hora:hora
-
-})
-
-localStorage.setItem("agendamentos",JSON.stringify(agendamentos))
-
-let telefone="5582996692302"
-
-let mensagem=`Olá Patricia! Gostaria de agendar.
-
-Nome: ${nome}
-Serviço: ${servico}
-Data: ${data}
-Hora: ${hora}`
-
-let url=`https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`
-
-window.open(url,"_blank")
+}
 
 }
+
+document.getElementById("data").addEventListener("change",carregarHorarios)
